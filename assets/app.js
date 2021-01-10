eb766120641ef08ccf2fa400081f1ab8c5e186fa
w3cproject <w3cproject@gmail.com>
2021-01-10 14:58:02 +0500

    add js/css assets. Add initial migration

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+
+import $ from 'jquery';
+
+$(function () {
+   console.log('jquery works');
+});
---
+++ assets/auth.js
@@ -0,0 +1,8 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/components/auth.scss';
+
+import $ from 'jquery';
+
+$(function () {
+   console.log('jquery works');
+});
